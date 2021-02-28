82a8e6fe58b630ba1716437e7e03bc22bd07be65
Georgi Sabev <georgi.msabev@gmail.com>
2021-03-01 00:45:24 +0200

    web folder created and bootstrap files moved

--- web/common.php
+++ web/common.php
@@ -1,13 +1,16 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load .env variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+require_once __DIR__ . '/../Controller/RouteController.php';
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
 $routeController = new \Controller\RouteController($router);
 $routeController->matchRoute();
+
 $router->run();
